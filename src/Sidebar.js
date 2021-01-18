4ba8e4e6d69d7f9c532ec22aaa2058209235c362
Abdulloh <formygithubacc@gmail.com>
2021-01-19 02:45:11 +0500

    add Sidebar with Channel List

---
+++ src/Sidebar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Sidebar.css";
+import SidebarChannel from "./SidebarChannel";
+
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <h3>Developers Laboratory</h3>
+        <ExpandMoreIcon />
+      </div>
+
+      <div className="sidebar__channels">
+        <div className="sidebar__channelsHeader">
+          <div className="sidebar__header">
+            <ExpandMoreIcon />
+            <h4>Text Channels</h4>
+          </div>
+          <AddIcon className="sidebar__addChannel" />
+        </div>
+        <div className="sidebar__channelsList">
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
